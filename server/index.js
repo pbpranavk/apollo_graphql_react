b44ae7edb966628a614eb4daa1e87369edcc9ee7
Pranav Kumar <pbpranav24@gmail.com>
2021-03-30 23:11:41 +0530

    Add db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { PrismaClient } = require("@prisma/client");
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -34,23 +35,33 @@ const books = [
   },
 ];
 
+const prisma = new PrismaClient();
+
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    books: async (parent, args, context) => {
+      return context.prisma.book.findMany();
+    },
   },
   Mutation: {
-    createBook: () => {
-      const book = { title: "Book Title", author: "Book Author" };
-      return book;
+    createBook: async (parent, args, context) => {
+      // const book = { title: "Book Title", author: "Book Author" };
+      const newBook = context.prisma.book.create({
+        data: {
+          title: args.title,
+          author: args.author,
+        },
+      });
+      return newBook;
     },
   },
 };
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context: { prisma } });
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
